1a82c5a7f32ddd6e5b07b976c3fbbe44b8961cae
Noelia Donato <noeliadonato@gmail.com>
2020-02-28 21:36:33 -0300

    editar funcionando

--- main.js
+++ main.js
@@ -42,57 +42,53 @@ const mostrarUsers = () => {
                     modal.innerHTML =
                         `<h2>Edit Employee</h2>
                 <form class="form-modal">
-                  <label for="name">Name</label>
-                  <input type="hidden" name= "id" value="${users[ind].id}" />
 
+                  <label for="name">Name</label>
                   <input type="text" name="name" id="name" value="${users[ind].fullname}" />
-          
+
                   <label for="email"> Email </label>
-                  <input type="email" name="email" id="email" value="${users[ind].email}" />
-          
+                  <input type="email" name="email" id="email" value="${users[ind].email}" required/>
+
                   <label for="address"> Address </label>
                   <input type="text" name="address" id="address" value="${users[ind].address}" />
-          
+
                   <label for="phone"> Phone </label>
                   <input type="number" name="phone" id="phone" value="${users[ind].phone}" />
-          
+
                   <div class="botones-modal">
-                  
+
                     <button>Cancel</button>
                     <button type="submit" id="guardar">Save</button>
                   </div>
                 </form>`
-                    modal.classList.remove("noMostrar")
-                    console.log(element.id)
+                    modal.classList.remove("noMostrar");
 
-                    const botonEditar = getElementById("guardar")
-                    botonEditar.onclick = () = {
-                        let id = form.elements[0].value;
-                        let nombre = form.elements[1].value;
-                        let correo = form.elements[2].value;
-                        let direccion = form.elements[3].value;
-                        let telefono = form.elements[4].value;
+                    const botonEditar = document.getElementById("guardar");
+
+                    botonEditar.onclick = (e) => {
+                        debugger
+                        e.preventDefault();
+                        const form = document.forms[0];
+                        let nombre = form.elements[0].value;
+                        let correo = form.elements[1].value;
+                        let direccion = form.elements[2].value;
+                        let telefono = form.elements[3].value;
 
                         let usuarioEditado = {
-                            id: id,
                             fullname: nombre,
                             email: correo,
                             address: direccion,
                             phone: telefono
                         };
 
-    editUsers(usuarioEditado);
-    mostrarUsers();
+                        editUsers(users[ind].id, usuarioEditado);
+                        debugger
+                        modal.classList.add("noMostrar");
+                        mostrarUsers();
                     }
-
                 }
-
             });
-
-
-
         })
-
 }
 
 mostrarUsers()
@@ -107,27 +103,21 @@ const addUsers = (nuevoUser) => {
         .then(data => data.json())
         .then(userAgregado => console.log(userAgregado))
 
+
+        mostrarUsers();
 }
-
-
 
 // Funcion para editar el usuario
-const editUsers = (userEditado) => {
-
-    fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users/${userEditado.id}`, {
+const editUsers = (id, userEditado) => {
+    console.log(userEditado);
+    fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users/${id}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(usersEdit),
+        body: JSON.stringify(userEditado),
     })
-        .then(data => data.json())
-        .then(userEditado => console.log(userEditado))
-
-
+    .then(data => data.json())
+    .then(userEditado => console.log(userEditado));
 }
-
-
-
-
 
 //Funcion para borrar usuarios
 const deleteUsers = (id) => {
@@ -162,6 +152,7 @@ form.onsubmit = (e) => {
     let telefono = form.elements[3].value
 
     let nuevoUser = {
+        id: id,
         fullname: nombre,
         email: correo,
         address: direccion,
@@ -171,30 +162,3 @@ form.onsubmit = (e) => {
     addUsers(nuevoUser)
     mostrarUsers()
 }
-
-
-
-// Filtro
-
-const filtro = document.getElementById("filter")
-
-// const btnEdit = document.getElementById("btnEdit");
-// console.log(btnEdit)
-
-
-
-
-
-
-// (pepito) => {
-//     console.log(pepito)
-//     modal.classList.remove("noMostrar");
-
-
-// }
-
-// const botonDelete = document.getElementById("btnDelete");
-
-
-
-
