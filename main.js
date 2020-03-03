ec393965eb56732cbd9dd07aca2271c53b297d3a
Noelia Donato <noeliadonato@gmail.com>
2020-03-02 21:22:59 -0300

    unificamos código repetido

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const usersTable = (users) => {
-    const contenedorTabla = document.getElementById("container-table")
+    const contenedorTabla = document.getElementById("container-table");
     const datosTabla = users.reduce((acc, curr) => {
         return acc + `
         <tr>
@@ -10,82 +10,43 @@ const usersTable = (users) => {
             <td>${curr.phone}</td>
             <td>
             <button class="botonEdit" id="${curr.id}"><i class="material-icons icon-edit" title="Edit">&#xE254;</i></button>
-            <button class="botonDelete" id="btn-${curr.id}"><i class="material-icons icon-delete"
-         title="Delete">&#xE872;</i></button></td>
+            <button class="botonDelete" id="btn-${curr.id}"><i class="material-icons icon-delete" title="Delete">&#xE872;</i></button></td>
         </tr>`
-
-    }, '')
+    }, '');
 
     contenedorTabla.innerHTML = `<table>
-        <tr>
-        <th><input type="checkbox" name="main" id=""></th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Adress</th>
-        <th>Phone</th>
-        <th>Actions</th>
-        </tr>
-        ${datosTabla}
-        </table>`
+                                    <thead>
+                                        <tr>
+                                            <th><input type="checkbox" name="main" id=""></th>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                            <th>Adress</th>
+                                            <th>Phone</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        ${datosTabla}
+                                    </tbody>
+                                </table>`
 
     const btnEdit = document.querySelectorAll(".botonEdit");
     const btnDelete = document.querySelectorAll(".botonDelete");
 
     btnEdit.forEach((element, ind) => {
         element.onclick = () => {
-            modal.classList.remove("noMostrar");
-            modal.innerHTML =
-                `<h2>Edit Employee</h2>
-                        <button type="button" id="closeModalEdit" data-dismiss="modal" aria-hidden="true">X</button>
-                <form class="form-modal">
+            modal.innerHTML = getModal(true, users, ind);
+            showModal(true);
 
-                  <label for="name">Name</label>
-                  <input type="text" minlength="5" maxlength="50" name="name" id="name" value="${users[ind].fullname}" required  />
-
-                  <label for="email"> Email </label>
-                  <input type="email" name="email" maxlength="60" id="email" value="${users[ind].email}" required />
-
-                  <label for="address"> Address </label>
-                  <input type="text" name="address" id="address" pattern= "[A-Za-z0-9 _-]+" value="${users[ind].address}" required />
-
-                  <label for="phone"> Phone </label>
-                  <input type="tel" name="phone" pattern="[0-9  -]+" id="phone" value="${users[ind].phone}" required />
-
-                  <div class="botones-modal">
-
-                    <button type="button" id="cancelEdit">Cancel</button>
-                    <button type="submit" id="guardar">Save</button>
-                  </div>
-                </form>`
-            const closeEditModal = document.getElementById("closeModalEdit")
-            closeEditModal.onclick = () => {
-                modal.classList.add("noMostrar")
-            }
-
-            const cancelEditModal = document.getElementById("cancelEdit")
-            cancelEditModal.onclick = (e) => {
-                e.preventDefault()
-                modal.classList.add("noMostrar")
-            }
-            
             const botonEditar = document.getElementById("guardar");
-            botonEditar.onclick = (e) => {
-                e.preventDefault();
+            botonEditar.onclick = () => {
                 const form = document.forms[0];
-                let nombre = form.elements[0].value;
-                let correo = form.elements[1].value;
-                let direccion = form.elements[2].value;
-                let telefono = form.elements[3].value;
+                form.onsubmit = (e) => {
+                    e.preventDefault()
 
-                let usuarioEditado = {
-                    fullname: nombre,
-                    email: correo,
-                    address: direccion,
-                    phone: telefono
-                };
-
-                editUsers(users[ind].id, usuarioEditado);
-                modal.classList.add("noMostrar");
+                    editUsers(users[ind].id, setUser(form));
+                    showModal(false);
+                }
             }
         }
     });
@@ -94,28 +55,28 @@ const usersTable = (users) => {
         element.onclick = () => {
             modal.innerHTML =
                 `<h2>Delete Employee</h2>
-                        <button type="button" id="closeModal" data-dismiss="modal" aria-hidden="true">X</button>
-                        <div class="bodyModal">
-                        Are you sure you want to delete these Records?
-                        This action cannot be undone.
-                        </div>
-                    <div class="footer">
+                <button type="button" id="closeModal" data-dismiss="modal" aria-hidden="true">X</button>
+                <div class="bodyModal">
+                    Are you sure you want to delete these Records?
+                    This action cannot be undone.
+                </div>
+                <div class="footer">
                     <button id="cancelDelete">Cancel</button>
                     <button type="submit" id="delete">Delete</button>
-                  </div>`
-            const closeModal = document.getElementById("closeModal")
+                </div>`;
+
+            const closeModal = document.getElementById("closeModal");
             closeModal.onclick = () => {
-                modal.classList.add("noMostrar")
+                modal.classList.add("noMostrar");
             }
 
-            const closeDeleteModal = document.getElementById("cancelDelete")
+            const closeDeleteModal = document.getElementById("cancelDelete");
             closeDeleteModal.onclick = (e) => {
-                e.preventDefault()
-                modal.classList.add("noMostrar")
+                e.preventDefault();
+                modal.classList.add("noMostrar");
             }
-            modal.classList.remove("noMostrar");
 
-            
+            modal.classList.remove("noMostrar");
             const botonDelete = document.getElementById("delete");
 
             botonDelete.onclick = (e) => {
@@ -146,14 +107,12 @@ const addUsers = (nuevoUser) => {
     })
         .then(data => data.json())
         .then(userAgregado => {
-            console.log(userAgregado)
             mostrarUsers();
         })
 }
 
 // Funcion para editar el usuario
 const editUsers = (id, userEditado) => {
-    console.log(userEditado);
     fetch(`https://tp-js-2-api-wjfqxquokl.now.sh/users/${id}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
@@ -161,7 +120,6 @@ const editUsers = (id, userEditado) => {
     })
         .then(data => data.json())
         .then(userEditado => {
-            console.log(userEditado)
             mostrarUsers();
         });
 }
@@ -174,75 +132,30 @@ const deleteUsers = (id) => {
     })
         .then(data => data.json())
         .then(usersDeleted => {
-            console.log(usersDeleted)
             mostrarUsers();
         })
 }
 
-const botonAddOpenModal = document.getElementById("add-user")
-const modal = document.getElementById("modal")
+const botonAddOpenModal = document.getElementById("add-user");
+const modal = document.getElementById("modal");
 
 botonAddOpenModal.onclick = () => {
-    modal.innerHTML = ` <h2>Add Employee</h2>
-    <button type="button" id="closeAddEmployee" data-dismiss="modal" aria-hidden="true">X</button>
-    <form class="form-modal">
-      <label for="name"> Name </label>
-      <input type="text" maxlength="50" name="name" id="name" value=""  required />
+    modal.innerHTML = getModal(false);
+    showModal(true);
 
-      <label for="email"> Email </label>
-      <input type="email" maxlength="60" name="email" id="email"  value=""  required/>
-
-      <label for="address"> Address</label>
-      <input type="text" name="address" pattern ="[A-Za-z0-9 _-]+" id="address"  value="" required />
-
-      <label for="phone">Phone </label>
-      <input type="tel" name="phone" pattern="[0-9  -]+" id="phone"  value="" required />
-
-      <div class="botones-modal">
-
-        <button type="button" id="cancelAdd">Cancel</button>
-        <button type="submit" id="addCerrar">Add</button>
-      </div>
-    </form>`
-    modal.classList.remove("noMostrar");
-    const closeAddModal = document.getElementById("closeAddEmployee")
-    closeAddModal.onclick = () => {
-        modal.classList.add("noMostrar")
-    }
-    
-    const botonCancelAdd = document.getElementById("cancelAdd")
-    botonCancelAdd.onclick = (e) => {
-        e.preventDefault()
-        modal.classList.add("noMostrar")
-    }
-
-    const botonAddCloseModal = document.getElementById("addCerrar")
+    const botonAddCloseModal = document.getElementById("addCerrar");
 
     botonAddCloseModal.onclick = () => {
-        
-        const form = document.forms[0]
+        const form = document.forms[0];
         form.onsubmit = (e) => {
-            e.preventDefault()
-            let nombre = form.elements[0].value
-            let correo = form.elements[1].value
-            let direccion = form.elements[2].value
-            let telefono = form.elements[3].value
-
-            let nuevoUser = {
-                fullname: nombre,
-                email: correo,
-                address: direccion,
-                phone: telefono
-            }
-
-            addUsers(nuevoUser)
-            modal.classList.add("noMostrar")
-           
+            e.preventDefault();
+            addUsers(setUser(form));
+            showModal(false);
         }
     }
 }
 
-const usuarioFiltrado = document.getElementById("filter")
+const usuarioFiltrado = document.getElementById("filter");
 
 // Funcion Filtrar
 usuarioFiltrado.onkeypress = e => {
@@ -252,5 +165,59 @@ usuarioFiltrado.onkeypress = e => {
             .then(users => {
                 usersTable(users);
             })
+    }
+}
+
+const setUser = (form) => {
+    return {
+        fullname: form.elements[0].value,
+        email: form.elements[1].value,
+        address: form.elements[2].value,
+        phone: form.elements[3].value
+    }
+}
+
+const getModal = (editabled, users, ind) => {
+
+    let modal =
+        `<h2>${editabled ? 'Edit' : 'Add'} Employee</h2>
+    <button type="button" id="close" data-dismiss="modal" aria-hidden="true">X</button>
+    <form class="form-modal">
+        <label for="name"> Name </label>
+        <input type="text" maxlength="50" name="name" id="name" value="${editabled ? users[ind].fullname : ''}"  required />
+
+        <label for="email"> Email </label>
+        <input type="email" name="email" maxlength="60" id="email" value="${editabled ? users[ind].email : ''}" required />
+
+        <label for="address"> Address</label>
+        <input type="text" name="address" pattern ="[A-Za-z0-9 _-]+" id="address"  value="${editabled ? users[ind].address : ''}" required />
+
+        <label for="phone">Phone </label>
+        <input type="tel" name="phone" pattern="[0-9  -]+" id="phone"  value="${editabled ? users[ind].phone : ''}" required />
+
+        <div class="botones-modal">
+            <button type="button" id="cancel">Cancel</button>
+            <button type="submit" id="${editabled ? 'guardar' : 'addCerrar'}">${editabled ? 'Save' : 'Add'}</button>
+        </div>
+    </form>`;
+
+    return modal;
+}
+
+const showModal = (show) => {
+    if (show) {
+        modal.classList.remove("noMostrar");
+        const closeAddModal = document.getElementById("close");
+        closeAddModal.onclick = () => {
+            showModal(false);
+        }
+
+        const botonCancelAdd = document.getElementById("cancel");
+        botonCancelAdd.onclick = (e) => {
+            e.preventDefault();
+            showModal(false);
+        }
+    } else {
+        modal.classList.add("noMostrar");
     }
 }
